ed21c9da3d9a99d3459b01337d8fca4f9bed44d6
Thomas <thomas.tilkema@tnt-digital.com>
2017-04-21 11:48:31 +0200

    Added Typescript definition file

---
+++ index.d.ts
@@ -0,0 +1,31 @@
+// Type definitions for detectIt
+// Project: https://github.com/rafrex/detect-it
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.1
+
+import detectHover from 'detect-hover';
+import detectPassiveEvents from 'detect-passive-events';
+import detectPointer from 'detect-pointer';
+import detectTouchEvents from 'detect-touch-events';
+
+declare namespace detectIt
+{
+  const deviceType: string = 'mouseOnly' | 'touchOnly' | 'hybrid';
+  const hasMouse: boolean;
+  const hasTouch: boolean;
+  const maxTouchPoints: number;
+  const passiveEvents: boolean;
+  const primaryHover: string = 'hover' | 'none';
+  const primaryPointer: string = 'fine' | 'coarse' | 'none';
+
+  const state = {
+    detectHover: detectHover,
+    detectPassiveEvents: detectPassiveEvents,
+    detectPointer: detectPointer,
+    detectTouchEvents: detectTouchEvents,
+  }
+
+  const update: () => void;
+}
+
+export default detectIt;
