44357d52912752d51ec116e5863c3f2309b00612
Rafael Pedicini <rafael@rafgraph.dev>
2020-10-17 14:18:24 -0400

    Update detection and remove rawTests export

--- src/index.ts
+++ src/index.ts
@@ -1,88 +1,104 @@
-export { supportsPassiveEvents } from 'detect-passive-events';
+interface noWindow {
+  screen: {
+    width?: number;
+    height?: number;
+  };
+  navigator: {
+    maxTouchPoints?: number;
+    userAgent?: string;
+  };
+  addEventListener?: () => void;
+  removeEventListener?: () => void;
+  matchMedia?: () => { matches: boolean };
+}
 
 // so it doesn't throw if no window or matchMedia
-const w = window || { screen: {}, navigator: {} };
-const mm = w.matchMedia || (() => ({ matches: false }));
+const w: Window | noWindow = typeof window !== 'undefined' ? window : { screen: {}, navigator: {} };
+const matchMedia = w.matchMedia || (() => ({ matches: false }));
 
-// rawTests, results used for main detects
-export const rawTests = {
-  touchEvents: {
-    ontouchstartInWindow: 'ontouchstart' in w,
-    TouchEventInWindow: 'TouchEvent' in w,
-  },
-  pointerEvents: {
-    PointerEventInWindow: 'PointerEvent' in w,
-    maxTouchPoints: w.navigator.maxTouchPoints || 0,
-  },
-  hover: {
-    hover: mm('(hover: hover)').matches,
-    none: mm('(hover: none)').matches,
-    anyHover: mm('(any-hover: hover)').matches,
-    anyNone: mm('(any-hover: none)').matches,
-  },
-  pointer: {
-    fine: mm('(pointer: fine)').matches,
-    coarse: mm('(pointer: coarse)').matches,
-    anyFine: mm('(any-pointer: fine)').matches,
-    anyCoarse: mm('(any-pointer: coarse)').matches,
+// passive events test
+// adapted from https://github.com/WICG/EventListenerOptions/blob/gh-pages/explainer.md
+let passiveOptionAccessed = false;
+const options = {
+  get passive() {
+    return (passiveOptionAccessed = true);
   },
 };
+// have to set and remove a no-op listener instead of null
+// (which was used previously), because Edge v15 throws an error
+// when providing a null callback.
+// https://github.com/rafgraph/detect-passive-events/pull/3
+const noop = () => {};
+w.addEventListener && w.addEventListener('p', noop, options);
+w.removeEventListener && w.removeEventListener('p', noop, false);
 
-// ontouchstartInWindow is the old-old-legacy way to determine a touch device (think first few years of touch devices)
-// and many websites interpreted it to mean that the device is a touch only phone
-// so chromium on a windows computer with a touch screen (primary input mouse) have ontouchstartInWindow as false
-// even though it supports TouchEvents, so need to check TouchEventInWindow as well for TouchEvent support,
-export const supportsTouchEvents =
-  rawTests.touchEvents.ontouchstartInWindow || rawTests.touchEvents.TouchEventInWindow;
+export const supportsPassiveEvents: boolean = passiveOptionAccessed;
 
-export const supportsPointerEvents = rawTests.pointerEvents.PointerEventInWindow;
+export const supportsPointerEvents: boolean = 'PointerEvent' in w;
 
-// if browser supports PointerEvents use is PointerEvents maxTouchPoints,
-// otherwise use TouchEvents, but note that some browsers may support
-// the TouchEvents api (TouchEventInWindow true) even when not running on a touch device,
-// but ontouchstartInWindow will still be false, so only use ontouchstartInWindow
-const hasTouch =
-  rawTests.pointerEvents.maxTouchPoints > 0 || rawTests.touchEvents.ontouchstartInWindow;
+const onTouchStartInWindow = 'ontouchstart' in w;
+const touchEventInWindow = 'TouchEvent' in w;
+// onTouchStartInWindow is the old-old-legacy way to determine a touch device
+// and many websites interpreted it to mean that the device is a touch only phone,
+// so today browsers on a desktop/laptop computer with a touch screen (primary input mouse)
+// have onTouchStartInWindow as false (to prevent from being confused with a
+// touchOnly phone) even though they support the TouchEvents API, so need to check
+// both onTouchStartInWindow and touchEventInWindow for TouchEvent support,
+// however, some browsers (chromium) support the TouchEvents API even when running on
+// a mouse only device (touchEventInWindow true, but onTouchStartInWindow false)
+// so the touchEventInWindow check needs to include an coarse pointer media query
+export const supportsTouchEvents: boolean =
+  onTouchStartInWindow || (touchEventInWindow && matchMedia('(any-pointer: coarse)').matches);
 
+const hasTouch = (w.navigator.maxTouchPoints || 0) > 0 || supportsTouchEvents;
+
+// userAgent is used as a backup to correct for known device/browser bugs
+// and when the browser doesn't support interaction media queries (pointer and hover)
+// see https://caniuse.com/css-media-interaction
 const userAgent = w.navigator.userAgent || '';
-
-// needed as a fallback when media query interaction features are not supported by the browser
-// https://caniuse.com/css-media-interaction
-const isAndriod = /android/.test(userAgent.toLowerCase());
 
 // iPads now support a mouse that can hover, however the media query interaction
 // feature results always say iPads only have a coarse pointer that can't hover
-// even when a mouse is connected (anyFine and anyHover are always false), unfortunately
-// the media query results don't indicate a hybrid device, which iPads should be classified as,
-// so determine if it is an iPad so can indicate it should be treated as a hybrid device with anyHover true
+// even when a mouse is connected (anyFine and anyHover are always false),
+// this unfortunately indicates a touch only device but iPads should
+// be classified as a hybrid device, so determine if it is an iPad
+// to indicate it should be treated as a hybrid device with anyHover true
 const isIPad =
-  rawTests.pointer.coarse &&
+  matchMedia('(pointer: coarse)').matches &&
   // both iPad and iPhone can "request desktop site", which sets the userAgent to Macintosh
   // so need to check both userAgents to determine if it is an iOS device
   // and screen size to separate iPad from iPhone
-  (/iPad/.test(userAgent) || /Macintosh/.test(userAgent)) &&
+  /iPad|Macintosh/.test(userAgent) &&
   Math.min(w.screen.width || 0, w.screen.height || 0) >= 768;
 
 const hasCoarsePrimaryPointer =
-  rawTests.pointer.coarse ||
-  // if the pointer is not coarse and not fine then the browser doesn't support media query interaction features
-  // so if isAndroid assume it has a coarse primary pointer, otherwise assume it doesn't
-  // note that all iOS devices support media query interaction features
-  (!rawTests.pointer.fine && isAndriod);
+  (matchMedia('(pointer: coarse)').matches ||
+    // if pointer is not coarse and not fine then the browser doesn't support
+    // interaction media queries, so if it's an android assume it has a coarse primary pointer
+    (!matchMedia('(pointer: fine)').matches && /android/i.test(userAgent))) &&
+  // bug in firefox on hybrid windows devices where the interaction media queries
+  // always indicate a touch only device (only has a coarse pointer that can't hover)
+  // so assume that the primary pointer is not coarse for firefox windows
+  !/Windows.*Firefox/.test(userAgent);
 
 const hasAnyHoverOrAnyFinePointer =
-  rawTests.pointer.anyFine ||
-  rawTests.hover.anyHover ||
-  // if no anyFine, no anyHover, and no anyCoarse then browser doesn't support media query interaction features
-  // so if not isAndroid then assume has anyFine or anyHover, otherwise assume it doesn't
-  (!rawTests.pointer.anyCoarse && !isAndriod) ||
-  // iPad's might have an input device that can hover, so assume it has anyHover
-  isIPad;
+  matchMedia('(any-pointer: fine)').matches ||
+  matchMedia('(any-hover: hover)').matches ||
+  // iPads might have an input device that can hover, so assume it has anyHover
+  isIPad ||
+  // if no onTouchStartInWindow then the browser is indicating that it is not a touch only device
+  // see above note for supportsTouchEvents
+  !onTouchStartInWindow;
 
-// a hybrid device is one that both hasTouch and any input can hover or has a fine pointer
+// a hybrid device is one that both hasTouch and
+// any input can hover or has a fine pointer, or the primary pointer is not coarse
 // if it's not a hybrid, then if it hasTouch it's touchOnly, otherwise it's mouseOnly
 export const deviceType: 'mouseOnly' | 'touchOnly' | 'hybrid' =
-  hasTouch && hasAnyHoverOrAnyFinePointer ? 'hybrid' : hasTouch ? 'touchOnly' : 'mouseOnly';
+  hasTouch && (hasAnyHoverOrAnyFinePointer || !hasCoarsePrimaryPointer)
+    ? 'hybrid'
+    : hasTouch
+    ? 'touchOnly'
+    : 'mouseOnly';
 
 export const primaryInput: 'mouse' | 'touch' =
   deviceType === 'mouseOnly'
